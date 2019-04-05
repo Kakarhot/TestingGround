9a2cdb70bd3ec4df0830bfa6770f9d4c2ebde970
chenwenshuo <chenwenshuo@126.com>
2019-04-05 14:07:09 +0800

    Composition over Inheritance

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -3,19 +3,27 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRouteComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
 
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 
-	auto ControlledPawn = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
-	if (ControlledPawn) 
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRouteComponent>();
+	if (!PatrolRoute) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
 	{
-		BlackboardComp->SetValueAsObject(NextWaypointKey.SelectedKeyName, ControlledPawn->PatrolPointsCPP[Index]);
-		BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, (Index + 1) % sizeof(ControlledPawn->PatrolPointsCPP));
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
 	}
+
+	BlackboardComp->SetValueAsObject(NextWaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, (Index + 1) % PatrolPoints.Num());
 
 	return EBTNodeResult::Succeeded;
 }
--- Source/TestingGrounds/Public/PatrollingGuard.h
+++ Source/TestingGrounds/Public/PatrollingGuard.h
@@ -15,8 +15,7 @@ class TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
-	TArray<AActor*> PatrolPointsCPP;
+
 	
 	
 };
